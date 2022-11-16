1d05fe821f9e5de29e6a83f79ccab5b13952411c
shreyashdj <shreyashjadhav1799@gmail.com>
2022-11-16 20:47:18 +0530

    [Shreyash]: Added code to find the 2nd largest and the 2nd smallest element with sorting the array

---
+++ Arrays/SecondLargestSmallestWithSort.js
@@ -0,0 +1,76 @@
+/* 
+    1. Write a program in the following steps
+    a. Generates 10 Random 3 Digit number.
+    b. Store this random numbers into a array.
+    c. Then find the 2nd largest and the 2nd smallest element without sorting the array.
+    Extend the above program to sort the array and then find the 2nd largest
+    and the 2nd smallest element.
+*/
+
+let numberArray = [];
+
+/*
+    function to generate random 3 digit number
+*/
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 900 + 100);
+}
+
+//  Pushing elements in array
+console.log("\n Ten random 3 digit numbers in Array: ")
+for (let i = 0; i < 10; i++) {
+    numberArray.push(generateRandomNumber());
+}
+console.log(numberArray);                           // printing 10 random numbers of 3-digit
+
+let largestNumber = 100;
+let smallestNumber = 999;
+let secondLargestNumber = 100;
+let secondSmallestNumber = 999;
+
+/*
+    Finding Largest & Smallest element
+*/
+
+for (i = 0; i < numberArray.length; i++) {
+    let currentElement = numberArray[i];
+
+    if (currentElement > largestNumber) {
+        largestNumber = currentElement;
+    }
+
+    if (currentElement < smallestNumber) {
+        smallestNumber = currentElement;
+    }
+}
+
+//printing largest and smallest element
+console.log("\n Largest Element : " + largestNumber);
+console.log(" Smallest Element : " + smallestNumber);
+
+/*
+    Finding Second Largest & Second Smallest element
+*/
+
+for (i = 0; i < numberArray.length; i++) {
+    let currentElement = numberArray[i];
+
+    if (currentElement > secondLargestNumber && currentElement < largestNumber) {
+        secondLargestNumber = currentElement;
+    }
+
+    if (currentElement < secondSmallestNumber && currentElement > smallestNumber) {
+        secondSmallestNumber = currentElement;
+    }
+}
+
+// printing second largest and second smallest
+console.log(" Second Largest Element : " + secondLargestNumber);
+console.log(" Second Smallest Element : " + secondSmallestNumber + "\n");
+
+console.log(numberArray.sort());          // printing array after sort
+
+console.log("\n Smallest Element : " + numberArray[0]);
+console.log(" Second Smallest Element : " + numberArray[1]);
+console.log(" Second Largest Element : " + numberArray[numberArray.length-2]);
+console.log(" Largest Element : " + numberArray[numberArray.length-1] + "\n");
